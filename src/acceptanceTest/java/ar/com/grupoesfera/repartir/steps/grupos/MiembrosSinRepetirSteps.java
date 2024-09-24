814b64f6ed8e1e12df9d377ba803f173e8f93f2c
danielspacekGIT <danielspacek@gmail.com>
2024-09-24 19:08:41 -0300

    Implementar la prueba de aceptacion automatizada del primer escenario

--- src/acceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/MiembrosSinRepetirSteps.java
+++ src/acceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/MiembrosSinRepetirSteps.java
@@ -3,19 +3,46 @@ package ar.com.grupoesfera.repartir.steps.grupos;
 import ar.com.grupoesfera.repartir.steps.CucumberSteps;
 import io.cucumber.java.es.Cuando;
 import io.cucumber.java.es.Entonces;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElement;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class MiembrosSinRepetirSteps  extends CucumberSteps {
 
     @Cuando("el usuario crea un grupo con un unico miembro repetido")
     public void el_usuario_crea_un_grupo_con_miembros_repetidos() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+        var crearGruposButton = driver.findElement(By.id("crearGruposButton"));
+        crearGruposButton.click();
+
+        driver.findElement(By.id("nombreGrupoNuevoInput")).sendKeys("Asado esquizofrenico");
+
+        var miembrosInput = driver.findElement(By.id("miembrosGrupoNuevoInput"));
+        miembrosInput.sendKeys("Daniel");
+        miembrosInput.sendKeys(Keys.ENTER);
+        miembrosInput.sendKeys("Daniel");
+        miembrosInput.sendKeys(Keys.ENTER);
+
+        driver.findElement(By.id("guardarGrupoNuevoButton")).click();
+
+        var wait = new WebDriverWait(driver, 2);
+        wait.until(visibilityOfElementLocated(By.id("mensajesToast")));
     }
 
     @Entonces("no deberia crear el grupo con miembros repetidos")
     public void no_deberia_crear_el_grupo_con_miembros_repetidos() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        var wait = new WebDriverWait(driver, 2);
+        var mensajesToast = wait.withMessage("Mostro Toast")
+                .until(visibilityOfElementLocated(By.id("mensajesToast")));
+        wait.withMessage("Título del Toast es 'Error'")
+                .until(textToBePresentInElement(mensajesToast, "Error"));
+        assertThat(mensajesToast.getText())
+                .as("Descripción del Toast")
+                .contains("No se puede guardar");
     }
 
     @Cuando("el usuario crea un grupo con dos miembros llamados {string} y otro miembro llamado {string}")
